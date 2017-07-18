6a46a51adc8a98a6daca4bbe5df46aaf1fcd78e6
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-07-17 20:45:52 -0500

    Add import_export django module

--- lots/admin.py
+++ lots/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 from .models import Lot, Contact
+
+
+class LotResource(resources.ModelResource):
+    class Meta:
+        model = Lot
+
+
+class ContactResource(resources.ModelResource):
+    class Meta:
+        model = Contact
 
 
 class ContactInline(admin.TabularInline):
@@ -8,17 +20,19 @@ class ContactInline(admin.TabularInline):
     extra = 2
 
 
-class ContactAdmin(admin.ModelAdmin):
+class ContactAdmin(ImportExportModelAdmin):
     fields = ['name', 'phone_number', 'is_owner']
     list_display = ('name', 'phone_number', 'is_owner')
     list_filter = ['is_owner']
     search_fields = ['name']
+    resource_class = ContactResource
 
 
-class LotAdmin(admin.ModelAdmin):
+class LotAdmin(ImportExportModelAdmin):
     inlines = [ContactInline]
     list_display = ('code', 'address')
     search_fields = ['code', 'address']
+    resource_class = LotResource
 
 
 admin.site.register(Lot, LotAdmin)
