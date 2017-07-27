a626e97bdb8816ed46760c55ad402b64e391538a
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-07-26 19:37:52 -0500

    Fix how we calculate total to really account for deleted objects

--- revenue/admin.py
+++ revenue/admin.py
@@ -1,26 +1,25 @@
 from django.contrib import admin
 from django.core.exceptions import ValidationError
-from django.forms import BaseInlineFormSet, ModelForm
+from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from revenue.models import Receipt, FeeLine
 
 
-class FeeLinesInlineFormSet(BaseInlineFormSet):
+class FeeLinesInlineFormSet(forms.BaseInlineFormSet):
     def clean(self):
         super(FeeLinesInlineFormSet, self).clean()
 
         total = 0
         for form in self.forms:
             if not form.is_valid() or form.cleaned_data.get('DELETE'):
-                return # there are other errors in the form or the item was deleted
+                continue # there are other errors in the form or the item was deleted
             total += form.cleaned_data.get('amount', 0)
 
         self.instance.total_amount = total
-        print(self.instance)
 
 
-class FeeLineForm(ModelForm):
+class FeeLineForm(forms.ModelForm):
     def clean(self):
         if self.cleaned_data['date_start'] > self.cleaned_data['date_end']:
             raise ValidationError(_("Date start must be before date end"))
