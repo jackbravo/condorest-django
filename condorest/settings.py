418b4b4ce81011ed309ad36bccf7445f01de60fd
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-09-30 10:29:26 -0500

    Use postgres instead of sqlite

--- condorest/settings.py
+++ condorest/settings.py
@@ -89,8 +89,13 @@ WSGI_APPLICATION = 'condorest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'condopy',
+        'USER': 'condopy',
+        'PASSWORD': 'condopy',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
