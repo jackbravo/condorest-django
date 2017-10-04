c7e3e7e7fe38761c04268922daa4eba98f508679
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-10-04 14:55:19 -0500

    Allow bulk changing default fee for lots

--- lots/admin.py
+++ lots/admin.py
@@ -1,4 +1,7 @@
+from django import forms
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
 from .models import Lot, Contact, LotType
@@ -28,6 +31,36 @@ class LotAdmin(ImportExportModelAdmin):
     ordering = ['name']
     search_fields = ['name', 'address']
     resource_class = LotResource
+
+    actions = ['update_fee']
+
+    class UpdateFeeForm(forms.Form):
+        _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+        fee = forms.DecimalField()
+
+    def update_fee(self, request, queryset):
+        if 'update' in request.POST:
+            form = self.UpdateFeeForm(request.POST)
+
+            if form.is_valid():
+                fee = form.cleaned_data['fee']
+                queryset.update(default_fee=fee)
+
+                self.message_user(
+                    request,
+                    "Changed default fee on {} lots".format(queryset.count())
+                )
+                return HttpResponseRedirect(request.get_full_path())
+
+        else:
+            form = self.UpdateFeeForm(initial={
+                '_selected_action': request.POST.getlist(admin.ACTION_CHECKBOX_NAME),
+            })
+
+        return render(request, 'admin/lots_update_fee.html', context={
+            'form': form,
+            'lots': queryset,
+        })
 
 
 admin.site.register(LotType)
