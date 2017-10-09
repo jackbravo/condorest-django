2e3f5579299fdb090dc12c4d15029c40ac2822cf
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-10-09 12:48:48 -0500

    Remove phonenujmber_field from settings.py

--- condorest/settings.py
+++ condorest/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phonenumber_field',
     'month',
     'import_export',
     'django_extensions',
