dd8334860af317e0ac73bca1e1d926540176797c
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-10-16 21:07:10 -0500

    Add admin to ExpenseNote model

--- expense/admin.py
+++ expense/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from expense.models import ExpenseNote
+
+
+class ExpenseNoteAdmin(admin.ModelAdmin):
+    list_display = ['date', 'contact', 'account', 'amount']
+    readonly_fields = ['amount']
+    fields = ('date', 'contact', 'number', 'details', 'account', 'amount', 'save_in_ledger')
+
+
+admin.site.register(ExpenseNote, ExpenseNoteAdmin)
