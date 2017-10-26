13dccf8376bfc523b0f9612f17b98c4a0960f12b
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-10-26 16:32:54 -0500

    Show month_discount in each row instead of month_amount minus month_discount

--- static/js/create_receipt.js
+++ static/js/create_receipt.js
@@ -6,51 +6,45 @@ $(document).ready(function() {
         var discount = parse_decimal($('#id_discount').val());
         this.value = amount.toFixed(2);
 
+        // reset colors
+        var rows = $('.fee-row');
+        rows.removeClass('table-warning table-success');
+        $('.discount-row').removeClass('bg-danger');
+
         var balance = new BigNumber('0.00');
         var payment_total = new BigNumber('0.00');
-        $(".fee-row").each(function () {
+        rows.each(function () {
             var month_amount = parse_decimal($('.fee-amount', this).text());
-
-            // apply discount_rate
-            if (!discount_rate.isZero()) {
-                month_amount = month_amount.minus(discount_rate.times(month_amount));
-                $('.fee-discount', this).text(month_amount.toFormat(2));
-            } else {
-                $('.fee-discount', this).text('');
-            }
-
-            var payment = amount.greaterThanOrEqualTo(month_amount) ? month_amount : amount;
-            payment_total = payment_total.add(payment);
+            var month_discount = new BigNumber('0.00');
 
             // apply discount
-            var month_discount = new BigNumber('0.00');
-            if (payment.lessThan(month_amount) && !discount.isZero()) {
-                month_discount = discount.greaterThanOrEqualTo(month_amount.minus(payment)) ?
-                    month_amount.minus(payment) : discount;
-                month_amount = month_amount.minus(month_discount);
+            if (!discount_rate.isZero()) {
+                month_discount = discount_rate.times(month_amount);
+            } else if (!discount.isZero() && amount.lessThan(month_amount)) {
+                month_discount = discount.greaterThanOrEqualTo(month_amount.minus(amount)) ?
+                    month_amount.minus(amount) : discount;
                 discount = discount.minus(month_discount);
-                $('.fee-discount', this).text(month_amount.toFormat(2));
             }
-
-            // add classes to rows
-            if (payment.equals(month_amount)) {
-                $(this).addClass('table-success').removeClass('table-warning');
-            } else if (payment.isZero() && month_discount.isZero()) {
-                $(this).removeClass('table-success table-warning');
-            } else {
-                $(this).addClass('table-warning').removeClass('table-success');
-                if (!discount_rate.isZero() || !month_discount.isZero()) {
-                    $('.discount-row').addClass('bg-danger');
-                } else {
-                    $('.discount-row').removeClass('bg-danger');
-                }
-            }
+            month_amount = month_amount.minus(month_discount);
+            var payment = amount.greaterThanOrEqualTo(month_amount) ? month_amount : amount;
 
             amount = amount.minus(payment);
             balance = balance.add(month_amount).minus(payment);
+            payment_total = payment_total.add(payment);
+
+            // add classes to rows
+            if (payment.equals(month_amount)) {
+                $(this).addClass('table-success');
+            } else if (!payment.isZero()) {
+                $(this).addClass('table-warning');
+                if (!month_discount.isZero()) {
+                    $('.discount-row').addClass('bg-danger');
+                }
+            }
 
             $('.fee-balance', this).text(balance.toFormat(2));
             $('.fee-payment', this).text(payment.toFormat(2));
+            $('.fee-discount', this).text(month_discount.toFormat(2));
         });
         $('.fee-balance-total').text(balance.toFormat(2));
         $('.fee-payment-total').text(payment_total.toFormat(2));
