f0861ff6c817f1f683e69cf362336545ff3d9148
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-11-03 21:01:54 -0600

    Add a little more functionality to EntryAdmin

--- ledger/admin.py
+++ ledger/admin.py
@@ -4,7 +4,9 @@ from ledger.models import Account, Entry
 
 class EntryAdmin(admin.ModelAdmin):
     list_display = ['date', 'amount', 'details', 'debit_account', 'credit_account']
-    list_filter = ['date']
+    list_filter = ['date', 'debit_account', 'credit_account']
+    search_fields = ['details', 'debit_account__name', 'credit_account__name']
+    ordering = ['-date']
 
 
 admin.site.register(Entry, EntryAdmin)
