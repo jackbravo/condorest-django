7e4fd0110a2d038bc8fa2255ded712d7cdbc10e7
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-11-04 23:07:13 -0600

    Better AccountAdmin options

--- ledger/admin.py
+++ ledger/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from ledger.models import Account, Entry
 
 
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'created', 'updated']
+    list_filter = ['type']
+    search_fields = ['name', 'type']
+
+
 class EntryAdmin(admin.ModelAdmin):
     list_display = ['date', 'amount', 'details', 'debit_account', 'credit_account']
     list_filter = ['date', 'debit_account', 'credit_account']
@@ -10,4 +16,4 @@ class EntryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Entry, EntryAdmin)
-admin.site.register(Account)
+admin.site.register(Account, AccountAdmin)
